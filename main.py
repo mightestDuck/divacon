8cf4c23cb41b8f0f2cb830d6e1c3c8fd0ca63c08
Blazej Banaszewski <bkba@teknologisk.dk>
2020-04-04 22:04:52 +0200

    quicksort implemented

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from random import randint
 import numpy as np
 
 
-def binary_search(search_space: np.ndarray, target: int, ordered: bool = False) -> (int, int, list):
+def binary_search(search_space: list, target: int, ordered: bool = False) -> (int, int, list):
     if not ordered:
-        search_space = np.sort(search_space)
+        search_space = quicksort(list(search_space))
 
     i_low = 0
     i_high = len(search_space) - 1
@@ -22,13 +21,30 @@ def binary_search(search_space: np.ndarray, target: int, ordered: bool = False) 
     return i_mid, steps, search_space
 
 
+def quicksort(l: list) -> list:
+    if len(l) < 2:
+        return l
+
+    # partitioning
+    pivot = l[-1]
+    left, right = [], []
+    for e in l[:-1]:
+        if e <= pivot:
+            left += [e]
+        else:
+            right += [e]
+
+    # recursion
+    left = quicksort(left)
+    right = quicksort(right)
+    return left + [pivot] + right
+
+
 def main():
-    start = randint(1, 100)
-    end = start * 20
-    l = np.arange(start, end, 3)
+    l = np.random.randint(low=1, high=100, size=12)
     target = np.random.choice(l, 1)[0]
     print(f'Randomly generated list of size {len(l)}')
-    idx, steps, sorted_list = binary_search(search_space=l, target=target, ordered=False)
+    idx, steps, sorted_list = binary_search(search_space=list(l), target=target, ordered=False)
     print(f'target={target}, idx={idx}, list[idx]={l[idx]} found in {steps} steps.')
 
 
